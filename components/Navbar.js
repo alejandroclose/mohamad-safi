dc70dbdc31fb9bb41cf72a0a905fdfe23eb43611
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2020-10-06 00:04:35 +0200

    added animation to nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import {useSpring, animated} from 'react-spring'
 import styles from "../styles/Navbar.module.css";
+
 
 export default function Navbar() {
 
   const [navOpen, setNavOpen] = useState(0)
+  const slide = useSpring({
+    transform: navOpen ? 'translate3d(0,0,0)' : 'translate3d(100vw,0,0)'
+    });
 
   return (
     <>
@@ -12,14 +17,14 @@ export default function Navbar() {
       <main className={styles.main} />
         <button className={styles.hamburger} onClick={ () => setNavOpen(!navOpen)}><img src="../images/menu.png" className={styles.menu}></img></button>
     </div>
-    <div className={navOpen ? styles.nav : styles.nodisplay}>
+    <animated.div className={styles.nav} style={slide}>
     <div className={styles.links}>
           <h2 className={styles.anchor}><a href="#workflow">WORKFLOW</a></h2>
           <h2 className={styles.anchor}><a href="#projects">PROJECTS</a></h2>
           <h2 className={styles.anchor}><a href="#about">ABOUT</a></h2>
           <h2 className={styles.anchor}><a href="#contact">CONTACT</a></h2>
         </div>
-    </div>
+    </animated.div>
     </>
   );
 }

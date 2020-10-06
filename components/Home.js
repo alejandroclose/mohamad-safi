c97d7c321ff47fc675b54501812621a96e1673a5
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2020-10-06 23:26:32 +0200

    home v2 in mobile done

--- components/Home.js
+++ components/Home.js
@@ -1,27 +1,34 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-        HI THERE! I’M MOHAMAD SAFI
-        </h1>
-
-        <h1 className={styles.title}>
-          A UX/UI DESIGNER AND PSYCHOLOGIST
-        </h1>
-
-        <div className={styles.break}/>
-        <h1 className={styles.description}>
-        Helping people widen perspectives to grow inside and out is what drives me.
-        </h1>
-        <div className={styles.grid}>
-          <button className={styles.button_l}><a href="#projects">See selected projects</a></button>
-          <button className={styles.button_r}><a href="#workflow">Learn about my workflow</a></button>
+        <div className={styles.profilebox}>
+          <h1 className={styles.title}>
+            <div>
+              <div>Hi there! I’m </div>
+              <div>
+              Mohamad <b className={styles.highlight}>Safi</b>
+              </div>
+            </div>
+            <div>A UX Designer and Psychologist</div>
+          </h1>
+          <img src="/images/profile.png" className={styles.profile} />
         </div>
-        
+
+        <h2 className={styles.description}>
+          I help people widen perspectives to grow inside and out.
+        </h2>
+        <div className={styles.grid}>
+          <button className={styles.button_l}>
+            <a href="#projects">See selected projects</a>
+          </button>
+          <button className={styles.button_r}>
+            <a href="#workflow">Learn about my workflow</a>
+          </button>
+        </div>
       </main>
     </div>
-  )
+  );
 }
